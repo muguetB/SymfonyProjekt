b5defe0649dd8e5a9440d6ad6dc1b0db6b49f40e
muguetB <balbina@bi.com.pl>
2018-01-06 21:34:22 +0100

    strona glowna, dodawanie wydarzenia, edytowanie wydarzenia

--- src/MainBundle/Controller/MainController.php
+++ src/MainBundle/Controller/MainController.php
@@ -3,11 +3,19 @@
 namespace MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use EventsBundle\Manager\EventsManager;
 
 class MainController extends Controller {
 
-    public function indexAction() {
-        return $this->render('MainBundle:Default:index.html.twig');
+    public function indexAction($status) {
+        $eventsManager = new EventsManager();
+
+        return $this->render('MainBundle:Default:index.html.twig', array(
+                    'events' => $eventsManager->getUserEvents(1),
+                    'mainEvent' => $eventsManager->getMainEvent(1),
+                    'status' => $status
+                        )
+        );
     }
 
 }
