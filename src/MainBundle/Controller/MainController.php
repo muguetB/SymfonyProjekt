cd192696ac6d159fb14b16cd318890f7144771b1
muguetB <balbina@bi.com.pl>
2018-01-07 17:33:58 +0100

    poprawienie wyswietlania menu na wszystkich stronach

--- src/MainBundle/Controller/MainController.php
+++ src/MainBundle/Controller/MainController.php
@@ -4,32 +4,27 @@ namespace MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use EventsBundle\Service\EventsManager;
-use UserBundle\Service\UserManager;
 
 class MainController extends Controller {
 
     public function indexAction($status) {
         $eventsManager = new EventsManager();
-        $userManager = new UserManager();
 
         return $this->render('MainBundle:Default:index.html.twig', array(
                     'events' => $eventsManager->getUserEvents(1),
                     'mainEvent' => $eventsManager->getMainEvent(1),
-                    'status' => $status,
-                    'user' => $userManager->getUserInformation(1)
+                    'status' => $status
                         )
         );
     }
 
     public function changeEventAction($eventId) {
         $eventsManager = new EventsManager();
-        $userManager = new UserManager();
 
         return $this->render('MainBundle:Default:index.html.twig', array(
                     'events' => $eventsManager->getUserEvents(1),
                     'mainEvent' => $eventsManager->getEvent($eventId),
-                    'status' => 0,
-                    'user' => $userManager->getUserInformation(1)
+                    'status' => 0
                         )
         );
     }
--- src/UserBundle/Controller/UserController.php
+++ src/UserBundle/Controller/UserController.php
@@ -3,11 +3,16 @@
 namespace UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use UserBundle\Service\UserManager;
 
 class UserController extends Controller {
 
     public function indexAction() {
-        return $this->render('UserBundle:Default:user.html.twig');
+        $userManager = new UserManager();
+
+        return $this->render('UserBundle:Default:user.html.twig', array(
+                    'user' => $userManager->getUserInformation(1)
+        ));
     }
 
 }
